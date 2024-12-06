6fe16d6fcb05c31679fd753da2510d7848b75044
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-12-06 23:49:45 +0000

    verify access token

--- app/database/oauth2.py
+++ app/database/oauth2.py
@@ -1,5 +1,6 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from schemas import schemas
 
 # openssl rand -hex 32
 SECRET_KEY = "a78680504dddf2067a04e4b631fd64d15b341165ef4ab593912f5022e382226d"
@@ -16,3 +17,16 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("users_id")
+
+        if id is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
