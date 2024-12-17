1858df9f0666e7784e3954d021682847c3d9e6d7
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-12-17 16:07:34 +0000

    add hosts

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ from app.models import models
 from app.database.db import engine
 from app.routers import post, user, auth, vote
 from app.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
-# 
+#
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
